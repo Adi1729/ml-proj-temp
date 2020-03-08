993551128761c17feedce699c6cffa36b33d0072
adi1729 <adiyadav.1729@gmail.com>
2020-03-09 03:36:35 +0530

    metrics added

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,52 @@
-from sklearn import metrics
+from sklearn import metrics as skmetrics
 
 class ClassificationMetrics:
 
     def __init__(self):
 
-        self.metrics = {'accuracy':self.accuracy}
+        self.metrics = {'accuracy':self._accuracy,
+                        'f1':self._f1,
+                        'recall':self._recall,
+                        'precision':self._precision,
+                        'logloss':self._logloss,
+                        'auc':self._auc}
+
+
+    def __call__(self,metric,y_true,y_pred,y_proba):
+        if metric not in self.metrics:
+            raise Exception('Metric Not Implemented')
+
+        if metric == 'auc':
+            if y_proba is None:
+                raise Exception('y proba can not be none in case of auc')
+            else return self._auc(y_true=y_true,y_proba=y_proba)
+
+        else:   
+            return self.metrics[metric](y_true=y_true, y_pred=y_pred)
+
+    
+
 
     @staticmethod
+    def _accuracy(y_true,y_pred):
+        return skmetrics.accuracy_score(y_true = y_true, y_pred = y_pred)
+
+   @staticmethod
+    def _recall(y_true,y_pred):
+        return skmetrics.recall_score(y_true = y_true, y_pred = y_pred)
+
+   @staticmethod
+    def _precision(y_true,y_pred):
+        return skmetrics.precision_score(y_true = y_true, y_pred = y_pred)
+
+   @staticmethod
+    def _logloss(y_true,y_pred):
+        return skmetrics.logloss_score(y_true = y_true, y_pred = y_pred)
+
+   @staticmethod
+    def _auc(y_true,y_pred):
+        return skmetrics.auc_score(y_true = y_true, y_pred = y_pred)
+
+   @staticmethod
+    def _f1(y_true,y_pred):
+        return skmetrics.f1_score(y_true = y_true, y_pred = y_pred)
